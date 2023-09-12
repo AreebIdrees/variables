f9bb75e8eba0d8503ff3ad537108fd7234aa7da1
AreebIdrees <141308164+AreebIdrees@users.noreply.github.com>
2023-09-12 16:02:22 +0530

    Add files via upload

---
+++ practical1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int var1;
+cout<<"enter the age:";
+cin>>var1;
+string var2;
+cout<<"enter the name:";
+cin>>var2;
+float var3;
+cout<<"enter the score:";
+cin>>var3;
+char var4;
+cout<<"enter the grade:";
+cin>>var4;
+
+cout<<endl;
+
+cout<<"My age is "<<var1<<endl;
+cout<<"My name is "<<var2<<endl;
+cout<<"My score is "<<var3<<endl;
+cout<<"My grade is "<<var4<<endl;
+// cout<<sizeof(var1)<<endl;
+// cout<<sizeof(var2)<<endl;
+// cout<<sizeof(var3)<<endl;
+// cout<<sizeof(var4)<<endl;
+}
